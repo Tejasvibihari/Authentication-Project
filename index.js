bb44f8d8b47abca207ed8a30332f09e19e89fac6
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-01 12:56:48 +0530

    collect user information and save it to data base

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Mongoose } from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
 const app = express();
 const port = 3000;
 
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 // mongodb connection
-// Mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log("Database connected");
-//   })
-//   .catch((err) => {
-//     console.error(`Error connecting to database: ${err}`);
-//   });
+mongoose.connect("mongodb://localhost:27017/secretsDb")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to database: ${err}`);
+  });
 
+const users = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+const User = mongoose.model("User", users);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -34,19 +47,35 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
+  try {
+    // Check if the email already exists
+    const existingUser = await User.findOne({ email });
 
-  console.log(email);
-  console.log(password);
+    if (existingUser) {
+      return res.send('Email already exists. Try logging in.');
+    }
 
+    // Create a new user
+    const newUser = new User({
+      email: email,
+      password: password
+    });
+    await newUser.save();
 
+    // Log the result (optional)
+    console.log('User registered:', newUser);
+
+    res.render('secrets.ejs'); // Render your success view
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
+
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-
-  console.log(email);
-  console.log(password);
 });
 
 app.listen(port, () => {
